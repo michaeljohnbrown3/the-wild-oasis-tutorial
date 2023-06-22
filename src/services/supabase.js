b36ed4e79b4e0caf5c0c05b9e7d9f49ac12950c9
michaeljohnbrown3 <michaeljohnbrown3@gmail.com>
2023-06-22 16:45:46 -0400

    added supabase backend

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://uuqsuutekvsbbxgidgpc.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1cXN1dXRla3ZzYmJ4Z2lkZ3BjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc0NjM3MDYsImV4cCI6MjAwMzAzOTcwNn0.AFv6mzqt6YuRLrTP9KXvSzsVn-3N2TrfjOIWo-gLNdA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
